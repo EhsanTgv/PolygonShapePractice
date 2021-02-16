c50a2c920a5882c7860e3a1f8aeae48f6ed46036
EhsanTgv <EhsanTgv@gmail.com>
2021-02-16 22:29:28 +0330

    implement create shape by changing seekBar value

--- app/src/main/java/com/taghavi/polygonshape/MainActivity.kt
+++ app/src/main/java/com/taghavi/polygonshape/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.taghavi.polygonshape
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        anglesSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                anglesText.text = progress.toString()
+                if (progress > 2) {
+                    shape.setImageDrawable(PolygonalDrawable(Color.BLUE, progress))
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
     }
 }
